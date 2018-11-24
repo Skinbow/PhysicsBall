b95c080f4ee6ecd4df31d22046bcfe0c30bc5c7b
Mikhail <spyro.yakimenko@gmail.com>
2018-11-24 19:47:45 +0100

    Minor changes

--- build/source/PhysicsBall.java
+++ build/source/PhysicsBall.java
@@ -1,14 +1,30 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class PhysicsBall extends PApplet {
+
 int BallRadius;
 float BallX, BallY;
 float PBallX, PBallY;
 float XPull, YPull;
 float accelX, accelY;
-float BounceLoss = 1.1, RollLoss = 1.003, AirFrict = 1.0006;
+float BounceLoss = 1.1f, RollLoss = 1.003f, AirFrict = 1.0006f;
 boolean jump = false;
 boolean frame = true;
 
-void setup() {
-  size(800, 600);
+public void setup() {
+  
 
   BallRadius = 5;
   BallX = 400;
@@ -17,8 +33,8 @@ void setup() {
   PBallY = BallY;
   XPull = 0;
   YPull = 0;
-  accelX = 0.4;
-  accelY = 0.5;
+  accelX = 0.4f;
+  accelY = 0.5f;
 
   background(255);
   noStroke();
@@ -26,7 +42,7 @@ void setup() {
   ellipse(BallX, BallY, 2*BallRadius, 2*BallRadius);
 }
 
-void draw() {
+public void draw() {
   if (!(BallX == PBallX && BallY == PBallY)) {
     background(255);
     fill(0);
@@ -52,7 +68,7 @@ void draw() {
   XPull /= AirFrict;
   YPull /= AirFrict;
   if (BallY == height - BallRadius) XPull /= RollLoss;
-  if (abs(XPull) < 10e-3) XPull = 0;
+  if (abs(XPull) < 10e-3f) XPull = 0;
 
   if (jump) {
     if (BallY > height - BallRadius) {
@@ -69,7 +85,7 @@ void draw() {
   }
 }
 
-void keyPressed() {
+public void keyPressed() {
   if (key == 'a' || key == 'A') {
     XPull -= accelX;
   }
@@ -80,6 +96,16 @@ void keyPressed() {
     if (!jump) {
       jump = true;
       YPull = -20;
+    }
+  }
+}
+  public void settings() {  size(800, 600); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "PhysicsBall" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
     }
   }
 }
